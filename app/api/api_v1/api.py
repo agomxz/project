e08c83340c5b215d88eda793f3426d9bcb2896e9
Efren <zerg1997@gmail.com>
2024-11-13 16:37:43 -0800

    :rocket:

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.endpoints.health_check import router as health_check_router
-from app.api.api_v1.endpoints.users import router as users_router
+from backend.app.api.api_v1.endpoints.transaction import router as transaction_router
 
 api_router = APIRouter()
 api_router.include_router(health_check_router, tags=["Health_Check"])
-api_router.include_router(users_router, tags=["Users"])
+api_router.include_router(transaction_router, tags=["Transaction"])
